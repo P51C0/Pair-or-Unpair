77bf25f561ee4bf6bc71f5f750dcada98ee694eb
P51C0 <49995581+P51C0@users.noreply.github.com>
2022-02-15 16:19:22 -0300

    Update pair_unpair.py

--- pair_unpair.py
+++ pair_unpair.py
@@ -6,8 +6,6 @@ Welcome to pair or unpair
 """)
 
 def pair_unpair():
-	#int(sys.argv[1]) = int(input('Put a number between 1 and 1000: '))
-
 	if int(sys.argv[1]) >= 1001:
 		print(f'Error, the number {int(sys.argv[1])} is higher than 1000')
 
